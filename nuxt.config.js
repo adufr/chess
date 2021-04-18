d45103124a250a915f22c62fb9d8fa560e2b951c
Arthur Dufour <arthur@road-b-score.com>
2021-04-18 17:00:52 +0200

    chore(project-name): rename

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'chess-projects',
+    title: 'Chess | arthurdufour.com',
     htmlAttrs: {
       lang: 'en'
     },
